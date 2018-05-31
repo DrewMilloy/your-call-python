a1730085c0837f3af753780be931ffafab84987f
Drew Milloy <Drew.Milloy@dyson.com>
2018-05-31 17:59:00 +0100

    Refactor to run commands only on real Pi

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 
-# import RPi.GPIO as GPIO 
-# GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
-# GPIO.setup(PLAY_BUTTON, GPIO.IN)
-# GPIO.setup(RECORD_BUTTON, GPIO.IN)
+import platform
 
-import keyboard 
+run_commands = 1
+
+if platform.machine() == 'x86_64':
+    from fakegpio import GPIO
+    run_commands = 0
+else:
+    import RPi.GPIO as GPIO 
+    
+PLAY_BUTTON = 24
+RECORD_BUTTON = 25
+
+GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
+GPIO.setup(PLAY_BUTTON, GPIO.IN)
+GPIO.setup(RECORD_BUTTON, GPIO.IN)
+
 import os
 import random
 from datetime import datetime
 from time import sleep     # this lets us have a time delay (see line 15)  
-PLAY_BUTTON = 24
-RECORD_BUTTON = 25
 
 RECORDING_DURATION = 15 # TODO make this 60 seconds
 
@@ -34,12 +43,12 @@ STATE_RECORDING_ENDED = 5
 state = STATE_WAITING
 
 def is_play_pressed():
-    # return GPIO.input(PLAY_BUTTON)
-    return keyboard.is_pressed('p')
+    return GPIO.input(PLAY_BUTTON)
+    # return keyboard.is_pressed('p')
     
 def is_record_pressed():
-    # return GPIO.input(RECORD_BUTTON)
-    return keyboard.is_pressed('r')
+    return GPIO.input(RECORD_BUTTON)
+    # return keyboard.is_pressed('r')
 
 def get_current_timestamp():
     current_datetime = datetime.utcnow()
@@ -51,15 +60,22 @@ def choose_a_file():
 
 def play_audio_file(file_to_play):
     play_command = COMMAND_PLAY.format(file_to_play)
-    print(play_command)
+    execute_command(play_command)
 
 def record_audio_file(file_to_record):
     record_command = COMMAND_RECORD.format(RECORDING_DURATION, file_to_record)
-    print(record_command)
+    execute_command(record_command)
+
+def execute_command(command):
+    if run_commands:
+        os.system(command)
+    else:
+        print(command)
+        sleep(1)
 
 try:
     while True:
-        while state == STATE_WAITING:            # this will carry on until you hit CTRL+C  
+        if state == STATE_WAITING:            # this will carry on until you hit CTRL+C  
             if is_play_pressed(): 
                 print ("Starting Playback")  
                 state = STATE_PLAYING
@@ -67,7 +83,7 @@ try:
                 print ("Starting Recording")
                 state = STATE_PROMPT_FOR_RECORDING
             sleep(0.1)         # wait 0.1 seconds  
-        if state == STATE_PLAYING:
+        elif state == STATE_PLAYING:
             # find a recording
             file_to_play = os.path.join(PATH_PLAYBACK, choose_a_file())
             # play the recording
@@ -75,7 +91,6 @@ try:
             state = STATE_PLAYBACK_ENDED
             # play some kind of tone
             play_audio_file(PATH_END_PLAYBACK_BEEP)
-            sleep(1)
             state = STATE_WAITING
         elif state == STATE_PROMPT_FOR_RECORDING:
             # play the record prompt
@@ -87,7 +102,6 @@ try:
             state = STATE_RECORDING_ENDED
             # play end tone
             play_audio_file(PATH_END_BEEP)
-            sleep(1)
             state = STATE_WAITING
 finally: 
     GPIO.cleanup()
